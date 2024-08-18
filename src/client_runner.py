3e92fcc20ffa78aff8a6db7db3449c9a447db9a3
mathkimchi <kingalvinjkim@gmail.com>
2024-08-18 05:14:03 -0400

    Update client_runner.py

--- src/client_runner.py
+++ src/client_runner.py
@@ -17,23 +17,23 @@ if __name__ == "__main__":  # if this is the file being run
     # else:
     #     port = int(port)
 
-    Client(("0.0.0.0", 8080)).run()
+    # Client(("0.0.0.0", 8080)).run()
 
-    # if GET_URL_LOCALLY:
-    #     with open("server_url.txt") as file:
-    #         data = file.read()
-    # else:
-    #     http = urllib3.PoolManager()
-    #     response = http.request(
-    #         "GET",
-    #         "https://raw.githubusercontent.com/mathkimchi/melody-maestros/master/server_url.txt",
-    #     )
-    #     data = response.data.decode("utf-8")
+    if GET_URL_LOCALLY:
+        with open("server_url.txt") as file:
+            data = file.read()
+    else:
+        http = urllib3.PoolManager()
+        response = http.request(
+            "GET",
+            "https://raw.githubusercontent.com/mathkimchi/melody-maestros/master/server_url.txt",
+        )
+        data = response.data.decode("utf-8")
 
-    # data = data.split("\n")
-    # url = data[0]
-    # port = int(data[1])
+    data = data.split("\n")
+    url = data[0]
+    port = int(data[1])
 
-    # print(f"connect to server: {url}:{port}")
+    print(f"connect to server: {url}:{port}")
 
-    # Client((url, int(port))).run()
+    Client((url, int(port))).run()
